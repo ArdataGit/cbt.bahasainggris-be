3af48ee8649c6df77cbcb6caf57e63d380d7a340
yudayuda78 <85638796+yudayuda78@users.noreply.github.com>
2026-03-16 12:56:12 +0700

    Fix : speaking audio

--- src/app.js
+++ src/app.js
@@ -5,11 +5,6 @@ import morgan from 'morgan';
 import routes from './routes/index.js';
 import authRoutes from './modules/auth/auth.routes.js';
 
-import { fileURLToPath } from 'url';
-import path from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -18,10 +13,9 @@ app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use('/api/uploads', express.static('src/public/uploads'));
+app.use('/uploads', express.static('src/public/uploads'));
 
-const uploadsPath = path.join(__dirname, 'public/uploads');
-app.use('/api/uploads', express.static(uploadsPath));
-app.use('/uploads', express.static(uploadsPath)); // Fallback for direct /uploads/ links
 
 app.use('/api', routes);
 app.use('/api/auth', authRoutes);
