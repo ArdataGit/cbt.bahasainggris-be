178667f5f4e9f8de300de090671bbc2590309cc5
yudayuda78 <85638796+yudayuda78@users.noreply.github.com>
2026-03-16 12:47:17 +0700

    Feat : path file

--- src/app.js
+++ src/app.js
@@ -5,6 +5,12 @@ import morgan from 'morgan';
 import routes from './routes/index.js';
 import authRoutes from './modules/auth/auth.routes.js';
 
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 app.set('trust proxy', true);
@@ -12,8 +18,10 @@ app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
-app.use('/api/uploads', express.static('src/public/uploads'));
-app.use('/uploads', express.static('src/public/uploads')); // Fallback for direct /uploads/ links
+
+const uploadsPath = path.join(__dirname, 'public/uploads');
+app.use('/api/uploads', express.static(uploadsPath));
+app.use('/uploads', express.static(uploadsPath)); // Fallback for direct /uploads/ links
 
 app.use('/api', routes);
 app.use('/api/auth', authRoutes);
